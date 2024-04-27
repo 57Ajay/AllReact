14c06cf5320a5973d127a38168e7df2370021ab6
Ajay Upadhyay <57ajay.u@gmail.com>
2024-04-27 13:23:44 +0530

    Using css modules to style components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import UserProfile from "./components/UserProfile";
 import './App.css';
+import Box from "./components/Box";
+
+
 const App =()=>{
   return(
     <Fragment>
       <UserProfile Username="Ajay" age={21} login={true} />
+      <Box />
     </Fragment>
   );
 };
---
+++ src/components/Box.jsx
@@ -0,0 +1,10 @@
+import './Box.css'
+const Box = ()=>{
+    return(
+        <div className="box">
+            <h1>Rectangular Box</h1>  
+        </div>
+    );
+};
+
+export default Box; 
