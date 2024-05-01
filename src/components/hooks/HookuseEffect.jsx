ccfc8f676284b784024423245473e4a24a6b41dc
Ajay Upadhyay <57ajay.u@gmail.com>
2024-05-01 23:45:01 +0530

    minor change

--- src/components/hooks/HookuseEffect.jsx
+++ src/components/hooks/HookuseEffect.jsx
@@ -2,13 +2,17 @@ import { Fragment, useEffect, useState } from "react";
 
 const HookuseEffect = () => {
     const [count, setCount] = useState(0);
+    const [sync, setSync] = useState(false);
+    useEffect(()=>{
+        document.title = "AllReact"+ " " +count;
+    },[sync, count])
     return (
         <Fragment>
-        {useEffect(()=>{
-        document.title = "AllReact"+ " " +count;
-    },[count])}
-    <button onClick={() => setCount(count + 1)}>Call useEffect</button>
-    <h3>useEffect Called {count} times.</h3>
+        
+    <button onClick={() => setSync((i)=> !i)}>Sync to Title</button>
+    <button onClick={() => setCount((c)=> c+1)}>Update Count</button>
+
+    <h3>Count is {count}.</h3>
     </Fragment>
     );
 };
