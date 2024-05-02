de69dff098a2331efaf85a74785e0bfe15d3848d
Ajay Upadhyay <57ajay.u@gmail.com>
2024-05-03 00:43:36 +0530

    creating some more API's

--- src/components/hooks/UseEffectHook.jsx
+++ src/components/hooks/UseEffectHook.jsx
@@ -4,13 +4,28 @@ import Box from "../Box/Box";
 
 const UseEffectHook = () => {
     const [users, setUsers] = useState([]);
-
+    const [images, setImages] = useState([]);
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then((response) => response.json())
             .then((data) => setUsers(data));
     }, []);
-
+    useEffect(()=>{
+        const fetchImg = async ()=>{
+            try{
+            const response = await fetch("https://jsonplaceholder.typicode.com/photos");
+            const data = await response.json();
+            console.log(data);
+            // sort the array randomly
+            data.sort(() => Math.random() - 0.5);
+            // fetching only 50 images Randomly
+            setImages(data.slice(0, 50));
+            }catch(error){ 
+                console.log(error);
+            }
+        };
+        fetchImg();
+    }, []);
     return (
         <Box className="container">
             {users.map((user) => (
@@ -24,8 +39,15 @@ const UseEffectHook = () => {
                     </ul>
                 </div>
             ))}
+            <br />
+            {images.map((image) => (
+                <div key={image.id} className="image-box">
+                    <img src={image.thumbnailUrl} alt={image.title} style={{width: "100px", height: "100px"}} />
+                </div>
+            ))}
         </Box>
     );
 };
 
 export default UseEffectHook;
+
