4fc70d42ad5ff95d70ad8abb86eafe5477576dc0
Ajay Upadhyay <57ajay.u@gmail.com>
2024-05-02 18:36:06 +0530

    creating API using fetch Api

--- src/components/hooks/HookuseEffect.jsx
+++ src/components/hooks/HookuseEffect.jsx
@@ -4,15 +4,28 @@ const HookuseEffect = () => {
     const [count, setCount] = useState(0);
     const [sync, setSync] = useState(false);
     useEffect(()=>{
-        document.title = "AllReact"+ " " +count;
-    },[sync, count])
+        document.title = "AllReact | useEffect";
+    },[sync]);
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/users", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then((response)=> response.json()   
+        .then((data)=> data.map((user)=> console.log(user.name)))
+        .catch((error)=> console.log(error)));
+    }, []);
     return (
-        <Fragment>
+    <Fragment>
+        <button onClick={() => setSync((i)=> !i)}>Sync to Title</button>
+        <button onClick={() => setCount((c)=> c+1)}>Update Count</button>
+        <h3>Count is {count}.</h3>
+        <h1>
+            {sync ? document.title = <h3>AllReact {count}</h3> : "AllReact"}
+        </h1>
         
-    <button onClick={() => setSync((i)=> !i)}>Sync to Title</button>
-    <button onClick={() => setCount((c)=> c+1)}>Update Count</button>
-
-    <h3>Count is {count}.</h3>
     </Fragment>
     );
 };
