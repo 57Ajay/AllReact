e2cb9197e83edf4e11a17da0f40b6115305b0355
Ajay Upadhyay <57ajay.u@gmail.com>
2024-05-09 22:45:04 +0530

    Setting up User-Details route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import  { Fragment } from "react";
 import UserProfile from "./components/UserProfile/UserProfile";
 import './App.css';
 import Box from "./components/Box/Box";
@@ -6,13 +6,13 @@ import UserFavoriteMovies from "./components/UserMovies/UserFavoriteMovies";
 import LoginForm from "./components/Forms/LoginForm";
 import DisplaySize from "./components/DisplaySize/DisplaySize";
 import RegisterForm from "./components/Forms/RegisterForm";
-import UserDetailsRenderer from "./components/UserDetails/UserDetailsRenderer";
 import UseEffectHook from "./components/hooks/UseEffectHook";
 import Requests from "./components/hooks/Requests";
 import CustomHook from "./components/CustomHooks/CustomHook";
 import useDocumentClicks from "../src/utils/hooks/useDocumentClicks"
 import PostContext from "./API/contexts/PostContext";
 import useFetchUser from "./components/CustomHooks/useFetchUser";
+import { Outlet } from "react-router-dom";
 const App =()=>{
   useDocumentClicks()
   const {user, loading, error} = useFetchUser(1);
@@ -39,9 +39,8 @@ const App =()=>{
         <RegisterForm />
         </Box>
       </Box>
-      <Box className="user-details">
-        <UserDetailsRenderer />
-      </Box>
+      
+      <Outlet />
       <br />
       <PostContext />
     </Fragment>
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
+import Box from './components/Box/Box.jsx'
+import UserDetailsRenderer from './components/UserDetails/UserDetailsRenderer.jsx'
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [{
+      path: 'user-details',
+      element:
+      <Box className="user-details">
+        <UserDetailsRenderer />
+      </Box>
+  }]
   },
-  
 ]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
