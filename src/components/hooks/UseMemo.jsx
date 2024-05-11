650c5d00d7fbc44f6d57ce958fbbe61303cb3fd9
Ajay Upadhyay <140410149+57Ajay@users.noreply.github.com>
2024-05-11 23:31:30 +0530

    added error handling

--- src/components/hooks/UseMemo.jsx
+++ src/components/hooks/UseMemo.jsx
@@ -1,26 +1,38 @@
-import { Fragment, useState, useMemo } from "react";
+import { Fragment, useState, useMemo, useCallback } from "react";
 
 const UseMemo = () => {
-    const [count, setCount] = useState(0);
     const [value, setValue] = useState(0);
+    const [count, setCount] = useState(0);
+    const [error, setError] = useState(false);
 
     const sum = useMemo(() => {
-        let total = 0;
-        for (let i = 1; i <= value; i++) {
-            total += i;
+        try {
+            return (value / 2) * (1 + value);
+        } catch (error) {
+            setError(true);
+            return 0;
         }
-        return total;
     }, [value]);
 
     const handleChange = (e) => {
-        setValue(e.target.value);
+        const parsedValue = parseInt(e.target.value);
+        if (isNaN(parsedValue) || parsedValue < 0) {
+            setValue(0);
+        } else {
+            setValue(parsedValue);
+        }
     };
+
+    const handleCount = useCallback(() => {
+        setCount(count + 1);
+    }, [count]);
 
     return (
         <Fragment>
-            <input type="number" value={value} onChange={handleChange} /><br /><br />
-            Sum is: {sum}
-            <button onClick={() => setCount(count + 1)}>Counter({count})</button>
+            <label htmlFor="inputValue">Value:</label>
+            <input type="number" id="inputValue" value={value} onChange={handleChange} /><br /><br />
+            {error ? <p>Error calculating sum</p> : <p>Sum is: {sum}</p>}
+            <button type="button" onClick={handleCount}>Counter({count})</button>
         </Fragment>
     );
 };
