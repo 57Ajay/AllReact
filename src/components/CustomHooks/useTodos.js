fd6edf73a121ee1650f2058fe1a57ca09994ce78
Ajay Upadhyay <140410149+57Ajay@users.noreply.github.com>
2024-05-12 11:07:33 +0530

    Create a custom useTodos.js hook

---
+++ src/components/CustomHooks/useTodos.js
@@ -0,0 +1,23 @@
+import { Fragment, useState } from 'react'
+import useTodos from '../customHooks/useTodos.js';
+ const UseEffect=()=> {
+  const [id, setId] = useState(1);
+  const todos = useTodos({id});
+  return (
+  <Fragment>
+    Id: {id}
+    <button onClick={() => setId(1)}>1</button>
+    <button onClick={() => setId(2)}>2</button>
+    <button onClick={() => setId(3)}>3</button>
+    <button onClick={() => setId(4)}>4</button>
+    <button onClick={() => setId(5)}>5</button>
+    {todos.map((todo)=>{
+      return <li key={todo.id}>  
+        {todo.title}<br />{todo.completed ? 'Completed' : 'Not Completed'}
+        </li>
+    })}
+  </Fragment>
+  );
+}
+
+export default UseEffect;
