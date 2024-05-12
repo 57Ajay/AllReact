85470f7af4a5c90881f9b3c8b377e228d7140d76
Ajay Upadhyay <140410149+57Ajay@users.noreply.github.com>
2024-05-12 11:10:10 +0530

    using customHook useTodos in UseEffect.jsx

--- src/components/hooks/UseEffect.jsx
+++ src/components/hooks/UseEffect.jsx
@@ -1,26 +1,18 @@
-import { Fragment, useEffect, useState } from 'react'
-import axios from 'axios';
+import { Fragment, useState } from 'react'
+import useTodos from '../customHooks/useTodos.js';
  const UseEffect=()=> {
-    const [todos, setTodos] = useState([]);
   const [id, setId] = useState(1);
-  useEffect(()=>{
-    setTimeout(()=>{
-      axios.get(`https://jsonplaceholder.typicode.com/todos?id=${id}`)
-      .then((response)=>{
-        setTodos(response.data);
-    });
-    }, 500)
-  }, [id]);
+  const todos = useTodos({id});
   return (
   <Fragment>
-    ID: {id}<br />
-    <button onClick={()=>setId(2)}>2</button>
-    <button onClick={()=>setId(3)}>3</button>
-    <button onClick={()=>setId(4)}>4</button>
+    Id: {id}
+    <button onClick={() => setId(1)}>1</button>
+    <button onClick={() => setId(2)}>2</button>
+    <button onClick={() => setId(3)}>3</button>
+    <button onClick={() => setId(4)}>4</button>
+    <button onClick={() => setId(5)}>5</button>
     {todos.map((todo)=>{
-      
-      return <li key={todo.id}>
-        
+      return <li key={todo.id}>  
         {todo.title}<br />{todo.completed ? 'Completed' : 'Not Completed'}
         </li>
     })}
